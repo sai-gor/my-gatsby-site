7fe25f762cda6b3a724f242642329e3c5cf0ea0b
sai-gor <sa-ooi@protonmail.com>
2022-09-24 01:08:34 +1000

    update layout and pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,8 +5,8 @@ import Layout from '../components/layout'
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
   )
 }
