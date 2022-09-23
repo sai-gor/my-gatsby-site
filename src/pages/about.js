d319616362a901688a04d97748ae3974310765bb
sai-gor <sa-ooi@protonmail.com>
2022-09-24 01:05:04 +1000

    create layout component and update all pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,13 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Me</h1>
-      <Link to="/">Back to Home</Link>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
   )
 }
 
